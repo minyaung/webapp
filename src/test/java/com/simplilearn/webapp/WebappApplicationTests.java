6f939cc94a543bd28715fdfde01336a49b1df05e
Min Yaung <yaungmin@gmail.com>
2021-06-09 06:31:32 -0700

    Switch to use chromedriver to test the webapp headless.

--- src/test/java/com/simplilearn/webapp/WebappApplicationTests.java
+++ src/test/java/com/simplilearn/webapp/WebappApplicationTests.java
@@ -7,20 +7,35 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebappApplicationTests {
 
     private static String Base_Url = "http://172.16.93.5:8080/webapp/";
     private WebDriver driver;
+    // private FirefoxOptions options;
 
     @Before
     public void setUp()
     {
-        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
-        driver = new FirefoxDriver();
-        driver.get(Base_Url);
+        // System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+	// options = FirefoxOptions();
+	// options.addArguments("--headless");
+        // driver = new FirefoxDriver(options);
+        // driver.get(Base_Url);
+
+        // Path chrome = Paths.get("/usr/bin/chromedriver");
+        // chrome.toFile().setExecutable(true);
+        // System.setProperty("webdriver.chrome.driver", chrome.toAbsolutePath().toString());
+
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions); 
+	driver.get(Base_Url);
     }
 
     @After
